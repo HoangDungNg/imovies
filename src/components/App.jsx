b0976f5771e4d23152f7a0d43a409acbaf6b7722
Hoang Dung <dungnguyen322003@gmail.com>
2022-12-25 15:42:33 +0800

    second commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Route, Switch } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <div>
+      <CssBaseline />
+      <h1>This is iMovies App</h1>
+    </div>
+  );
+};
+
+export default App;
