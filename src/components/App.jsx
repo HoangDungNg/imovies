1201a33f0ae79801703ec9b6b0b6ce3c48a4097e
Hoang Dung <dungnguyen322003@gmail.com>
2022-12-25 22:17:36 +0800

    file and folder structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
 import { CssBaseline } from '@mui/material';
 import { Route, Switch } from 'react-router-dom';
+import useStyles from './styles';
+
+import { Actors, MovieInformation, Movies, NavBar, Profile } from './';
 
 const App = () => {
+  const classes = useStyles();
+
   return (
-    <div>
+    <div className={classes.root}>
       <CssBaseline />
-      <h1>This is iMovies App</h1>
+      <NavBar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Switch>
+          <Route exact path="/movie/:id">
+            <MovieInformation />
+          </Route>
+          <Route exact path="/actors/:id">
+            <Actors />
+          </Route>
+          <Route exact path="/">
+            <Movies />
+          </Route>
+          <Route exact path="/profile/:id">
+            <Profile />
+          </Route>
+        </Switch>
+      </main>
     </div>
   );
 };
---
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MovieInformation = () => {
+  return <div>Movie Information</div>;
+};
+
+export default MovieInformation;
